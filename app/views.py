c43f5b91701e8fd0eda314fad5043ff832cb47dc
darksh3ll <darksh3ll.js@gmail.com>
2024-06-29 16:50:02 +0200

    wip

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from testimony.models import Testimonial
 def index(request):
+    title='lorem10'
     team_members = [
         {"name": "reinhard boonke", "title": "Evangeliste", "image": "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQsZEL1wDx5LhaUo9TEHAL2o-6YDMqvK5UEyP5HkzGHccm4JyCE"},
         {"name": "Yahya R. Makarim", "title": "President & CEO", "image": "https://images.pexels.com/photos/2897883/pexels-photo-2897883.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"},
@@ -10,7 +11,7 @@ def index(request):
         {"name": "Ahmad Sultani", "title": "chef groupe coral", "image": "https://images.pexels.com/photos/3778680/pexels-photo-3778680.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"},
     ]
     address = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d480.5462847513066!2d5.88154952529991!3d43.09426159991749!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12c9035c5ebb7e0d%3A0xdba473d9c39c9e3a!2sRue%20Alexandre%20Ghibaudo!5e0!3m2!1sen!2sid!4v1718898242201!5m2!1sen!2sid'
-    return render(request, 'index.html', {'team_members': team_members,'address':address})
+    return render(request, 'index.html', {'team_members': team_members,'address':address,'title':title})
 
 
 def donate(request):
--- testimony/views.py
+++ testimony/views.py
@@ -1,13 +1,10 @@
 from django.shortcuts import render
 from .models import Testimonial
 def testimony_view(request):
-    testimonials = Testimonial.objects.all()
-    for testimonial in testimonials:
-        print(testimonial)
 
     context = {
-        'testimonials': testimonials,
+        'title':"laurent"
     }
 
     
-    return render(request, 'testimony.html', context)
+    return render(request, 'testimony.html',context)
