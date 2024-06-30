bb9a6d867d3be73aad645ef5795f5fee858be7d2
darksh3ll <darksh3ll.js@gmail.com>
2024-06-30 18:52:44 +0200

    wip

--- fullCalendar/views.py
+++ fullCalendar/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+from .models import Event
+
+def calendar_view(request):
+    events = Event.objects.all()
+    events_list = []
+    for event in events:
+        events_list.append({
+            'title': event.title,
+            'start': event.start_time.isoformat(),
+            'end': event.end_time.isoformat(),
+        })
+    context = {
+        'events': events_list
+    }
+    return render(request, 'calendar_view.html', context)
