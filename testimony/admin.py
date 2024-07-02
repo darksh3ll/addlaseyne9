b65c043acaf3105a1e4d8296924b12569510f127
darksh3ll <darksh3ll.js@gmail.com>
2024-07-02 20:11:02 +0200

    wip

--- testimony/admin.py
+++ testimony/admin.py
@@ -3,6 +3,8 @@ from .models import Testimonial
 
 @admin.register(Testimonial)
 class TestimonialAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author_name', 'publication_date', 'text')
+    list_display = ('title', 'author_name', 'publication_date', 'text','is_active')
+    list_editable = ('is_active',)
+    prepopulated_fields = {"slug": ("title",)}  # new
     search_fields = ('title', 'author_name', 'text')
     list_filter = ('publication_date',)
--- testimony/models.py
+++ testimony/models.py
@@ -1,13 +1,26 @@
 from django.db import models
+from django.urls import reverse
+from django.template.defaultfilters import slugify  # new
 
 class Testimonial(models.Model):
     title = models.CharField(max_length=200, verbose_name="Title", blank=False)
+    slug = models.SlugField(null=False, unique=True)  # new
     author_name = models.CharField(max_length=100, verbose_name="Author's Name", blank=False)
     publication_date = models.DateField(verbose_name="Publication Date", blank=False)
     text = models.TextField(verbose_name="Testimonial Text", blank=False)
+    is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse("testimonial_detail", kwargs={"slug": self.slug})
+    
+
+    def save(self, *args, **kwargs):  # new
+        if not self.slug:
+            self.slug = slugify(self.title)
+        return super().save(*args, **kwargs)
 
     class Meta:
         verbose_name = "Témoignage"
