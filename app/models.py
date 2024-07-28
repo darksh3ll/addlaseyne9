84b692745c5445a497cfdd49b7301a7e99a2ace7
darksh3ll <darksh3ll.js@gmail.com>
2024-07-28 18:15:10 +0200

    wip

--- app/models.py
+++ app/models.py
@@ -34,3 +34,28 @@ class CarouselItem(models.Model):
 
     def __str__(self):
         return self.title
+    
+
+class ChurchInfo(models.Model):
+    name = models.CharField(max_length=255, verbose_name="Nom de l'église", blank=False)
+    street_number = models.CharField(max_length=10, verbose_name="Numéro de rue", blank=False)
+    street_name = models.CharField(max_length=255, verbose_name="Nom de la rue", blank=False)
+    city = models.CharField(max_length=255, verbose_name="Ville", blank=False)
+    postal_code = models.CharField(max_length=10, verbose_name="Code postal", blank=False)
+    country = models.CharField(max_length=255, verbose_name="Pays", blank=False)
+    phone = models.CharField(max_length=20, verbose_name="Téléphone", blank=False)
+    mobile_phone = models.CharField(max_length=20, verbose_name="Téléphone portable", blank=False)
+    rib_number = models.CharField(max_length=34, verbose_name="Numéro RIB", blank=False)
+    email = models.EmailField(verbose_name="Email", blank=False)  # Nouveau champ pour email
+    facebook = models.URLField(verbose_name="Facebook", blank=True)
+    instagram = models.URLField(verbose_name="Instagram", blank=True)
+    twitter = models.URLField(verbose_name="Twitter", blank=True)
+    youtube = models.URLField(verbose_name="YouTube", blank=True)  # Nouveau champ pour YouTube
+    google_maps_url = models.URLField(verbose_name="Google Maps URL", blank=True)  # Nouveau champ pour Google Maps
+
+    class Meta:
+        verbose_name = "Information de l'église"
+        verbose_name_plural = "Informations de l'église"
+
+    def __str__(self):
+        return self.name
