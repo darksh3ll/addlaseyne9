ed0da9546926a3523b8b3e7b4abcc7be67cdfaac
darksh3ll <darksh3ll.js@gmail.com>
2025-05-04 12:58:28 +0200

    wip

--- config/settings.py
+++ config/settings.py
@@ -124,6 +124,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 CSRF_TRUSTED_ORIGINS = [
+    'https://*.ngrok.io',
+    'https://*.ngrok-free.app',
     'https://51ce-93-28-68-57.ngrok-free.app',
     # Ajoutez d'autres URLs de confiance ici si nécessaire
 ]
