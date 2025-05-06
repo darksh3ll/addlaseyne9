6c3cbd56da9f823e9e76bf9328ba8f9f58be0deb
darksh3ll <darksh3ll.js@gmail.com>
2025-05-06 19:32:00 +0200

    add goole maps url

--- app/views.py
+++ app/views.py
@@ -13,19 +13,15 @@ def index(request):
     latest_video = LatestYouTubeVideo.objects.last()
     schedule = ChurchSchedule.objects.all()
     
-    address = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d480.5462847513066!2d5.88154952529991!3d43.09426159991749!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12c9035c5ebb7e0d%3A0xdba473d9c39c9e3a!2sRue%20Alexandre%20Ghibaudo!5e0!3m2!1sen!2sid!4v1718898242201!5m2!1sen!2sid'
-   
-   
     context = {
         'team_members': team_members,
-        'address': address,
         'title': title,
         'photo_gallery':photo_gallery,
         'carousel_items':carousel_items,
         'church_info': church_info,
         'stream':stream,
         'latest_video':latest_video,
-        "schedule": schedule
+        "schedule": schedule,
         
     }
 
